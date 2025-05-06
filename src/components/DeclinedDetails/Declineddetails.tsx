5e642ac5db1e8b2bb3b094b3dc53d283b024671e
adithya <adithyarana447@gmail.com>
2025-05-06 14:15:11 +0530

    changed attributes

--- src/components/DeclinedDetails/Declineddetails.tsx
+++ src/components/DeclinedDetails/Declineddetails.tsx
@@ -3,14 +3,18 @@
 const  Declineddetails=() =>{
   return (
     <div className="overflow-x-auto p-4 styled-scrollbar  your-div">
-    <div className="min-w-[1300px] h-[650px]  ">                                    
+    <div className="min-w-[2100px] h-[650px]  ">                                    
       <div className="grid grid-cols-[repeat(11,minmax(150px,1fr))] 2xl:grid-cols-[repeat(11,minmax(190px,1fr))] gap-x-12  font-semibold  w-full p-2 rounded-t text-nowrap bg-sky-50 ">
-        <div>Customer Name</div>
-        <div>BVN</div>
-        <div>Identity Card</div>
-        <div>Utility bill</div>
-        <div>Status</div>
-        <div>Submission Date</div>
+      <div>Driver Name</div>
+        <div>Aadhar ID</div>
+        <div>PAN ID</div>
+        <div>DL Number</div>
+        <div>License ID</div>
+        <div>Bank Account</div>
+        <div>Registration Cert..</div>
+        <div>Vehicle Number</div>
+        <div>Document</div>
+        <div>V2 Status</div>
         <div>Action</div>
       </div>
       </div>
